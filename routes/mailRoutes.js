192ea02d7d13d06b15698a5ee892c399838bb167
mozzam123 <mozzam607@gmail.com>
2024-02-04 19:55:00 +0530

    removed unwanted files

--- routes/mailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mailController = require("./../controllers/mailcontroller");
-
-router.route("/send-email").post(mailController.sendMail);
-router.route("/").get(mailController.getHomePage);
-
-module.exports = router;
